c7235d413f373efd6fdd1b94b4ae10ce1e90ec3d
Suja Venkatesan <suja.venkatesan@syncfusion.com>
2023-01-11 20:07:46 +0530

    MAUI-4255 : How to refresh the item in SfListView automatically at runtime using timer?

---
+++ ListViewMaui/ListViewMaui/Helper/Behavior.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace ListViewMaui
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+
+        private Syncfusion.Maui.ListView.SfListView listView;
+       
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            InitializeTimer();
+            listView = bindable.FindByName<Syncfusion.Maui.ListView.SfListView>("listView");
+            listView.PropertyChanged += ListView_PropertyChanged;
+            base.OnAttachedTo(bindable);
+        }
+        private void ListView_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "ItemsSource")
+                InitializeTimer();
+        }
+
+        private async void InitializeTimer()
+        {
+            await WaitAndExecute(2000, () =>
+            {
+                var viewmodel = new ContactInfoRepository();
+                listView.ItemsSource = viewmodel.ContactInfo;
+            });
+        }
+
+        private async Task WaitAndExecute(int milisec, Action actionToExecute)
+        {
+            await Task.Delay(milisec);
+            actionToExecute();
+        }
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            listView = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+    }
+}
---
+++ ListViewMaui/ListViewMaui/View/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace ListViewMaui;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
